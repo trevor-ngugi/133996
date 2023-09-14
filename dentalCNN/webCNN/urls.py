0fc061fd7f13cf8eaaece49bc37438ced4d397b0
trevor ngugi <ngugitrevor@gmail.com>
2023-09-14 13:23:25 +0300

    able to login and logout of users

--- dentalCNN/webCNN/urls.py
+++ dentalCNN/webCNN/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
      path("", views.index, name="index"),
-     path("homepage", views.homepage, name="home"),
-     path("login-page",views.login,name="login-page")
+    #  path("homepage", views.homepage, name="home"),
+     path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    #  path("login-page",views.login,name="login-page")
  ]
