3fc3544a89daaadda1aa5d113f9bb6fd0768d048
trevor ngugi <ngugitrevor@gmail.com>
2023-10-21 18:25:13 +0300

    edited the dental url

--- dentalCNN/dentalCNN/urls.py
+++ dentalCNN/dentalCNN/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/',include("webCNN.urls"))
+    path('dental/',include("webCNN.urls"))
 ]
